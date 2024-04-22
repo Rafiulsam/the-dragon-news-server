6bb8e8716618f89f3ce6eee31db25c7113035b1d
Rafiulsam <rafiulsam69@gmail.com>
2024-04-22 15:56:48 +0600

    created news data

--- index.js
+++ index.js
@@ -4,18 +4,42 @@ const app = express()
 const port = 5000
 
 const categories = require('./data/categories.json')
+const news = require('./data/news.json')
+
 const cors = require('cors')
 
 app.use(cors())
 
-app.get('/',(req, res)=>{
+app.get('/', (req, res) => {
     res.send('Dragon is running')
 })
 
-app.get('/categories', (req, res)=>{
+app.get('/categories', (req, res) => {
     res.send(categories)
 })
 
-app.listen(port,()=>{
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+
+app.get('/categories/:id', (req, res) => {
+    const id = req.params.id
+    if (id == 0) {
+        res.send(news)
+    }
+    else {
+        const categoryNews = news.filter(n => n.category_id === id)
+        res.send(categoryNews)
+    }
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id
+    console.log(id);
+    const selectedNews = news.find(n => n._id === id)
+    res.send(selectedNews)
+})
+
+app.listen(port, () => {
     console.log(`dragon server is running on port: ${port}`)
 })
